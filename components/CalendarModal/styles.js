a009deb26ae159ecdfb9163e7c6dbe9316898705
Fabricio Goveia <115665368+fffgoveia@users.noreply.github.com>
2023-05-24 16:50:57 -0300

    Add files via upload

---
+++ components/CalendarModal/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex:1;
+  background-color: rgba(34,34,34, 0.4);
+`;
+
+export const ModalContent = styled.View`
+  flex:2;
+  justify-content: center;
+  background-color: #FFF;
+  padding: 14px;
+`;
+
+export const ButtonFilter = styled.TouchableOpacity`
+  border-radius: 4px;
+  background-color: #3B3DBF;
+  height: 45px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonFilterText = styled.Text`
+  color: #FFF;
+  font-size: 19px;
+  font-weight: bold;
+`;
